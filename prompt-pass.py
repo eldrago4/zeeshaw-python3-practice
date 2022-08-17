ae67d56dddfc5a266f398fdcd3a08e1e3608ec82
eldrago <tred38434@gmail.com>
2022-08-18 01:41:59 +0530

    added comments

--- prompt-pass.py
+++ prompt-pass.py
@@ -3,14 +3,15 @@ from sys import argv
 ayana,user_name=argv
 prompt='$'
 
-print(f"Hi, {user_name} I'm {ayana}")
+print(f"Hi, {user_name} I'm {ayana}") # first arguement will always be file name
 print("I'd like to ask you few questions.. uwu")
 
 print(f"Do you like me {user_name}? [do/don't]")
-likes = input(prompt)
+likes = input(prompt) # creating a variable that stores input.
 
 print("Where do you live??")
-lives=input(prompt)
+lives=input(prompt) # we write x as message to be printed while fetching
+# user input in input(x)
 
 print("What computer do you use?")
 computer = input(prompt)
